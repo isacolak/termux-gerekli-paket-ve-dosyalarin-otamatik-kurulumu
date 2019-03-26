0987660f1f54393e721f26211e1fd578af83c6ae
isacolak <48714435+isacolak@users.noreply.github.com>
2019-03-26 18:22:05 +0300

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,4 @@
+mv /data/data/com.termux/files/home/termux-gerekli-paket-ve-dosyalarin-otamatik-kurulumu/termux-otamatik-paket-ve-dosya-kurulumu.py /data/data/com.termux/files/home/
+mv /data/data/com.termux/files/home/termux-gerekli-paket-ve-dosyalarin-otamatik-kurulumu/dosyalar.py /data/data/com.termux/files/home/
+
+python /data/data/com.termux/files/home/termux-otamatik-paket-ve-dosya-kurulumu.py
