ae6af89e89e8f3746c9fe2e18c8807daa12b18c8
isacolak <48714435+isacolak@users.noreply.github.com>
2019-03-26 19:52:48 +0300

    Delete install.sh

--- install.sh
+++
@@ -1,4 +0,0 @@
-mv /data/data/com.termux/files/home/termux-gerekli-paket-ve-dosyalarin-otamatik-kurulumu/termux-otamatik-paket-ve-dosya-kurulumu.py /data/data/com.termux/files/home/
-mv /data/data/com.termux/files/home/termux-gerekli-paket-ve-dosyalarin-otamatik-kurulumu/dosyalar.py /data/data/com.termux/files/home/
-
-python /data/data/com.termux/files/home/termux-otamatik-paket-ve-dosya-kurulumu.py
